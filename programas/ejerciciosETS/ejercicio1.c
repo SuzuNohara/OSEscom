1430bc7ba3d9c606ea874d93a624ac23ec784dc3
NinjatoTim <yamato.hikage@gmail.com>
2024-01-29 20:24:58 -0600

    Revert "ejercicio 14"
    This reverts commit ba5aea1bfd0b06c3de9a4e3156482a4448ba6dce.

---
+++ programas/ejerciciosETS/ejercicio1.c
@@ -0,0 +1,45 @@
+#include <sys/types.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+//codigo programa 1
+
+int main(int argc, char *argv[])
+{
+    pid_t pid1, pid2;
+    int status1, status2;
+
+    pid1 = fork();
+    // comprobación de que el primer hijo se creó sin errores
+    if (pid1 < 0){
+        perror("Error en fork 1");
+        exit(EXIT_FAILURE);
+    }
+
+    if (pid1 == 0){        
+        sleep(5);
+        exit(5);
+    }
+    // comprobación de que el segundo hijo se creó sin errores
+    pid2 = fork();
+    if (pid2 < 0){
+        perror("Error en fork 2");
+        exit(EXIT_FAILURE);
+    }
+
+    if (pid2 == 0){
+        sleep(1);
+        exit(1);
+    }
+
+    // Código del padre
+    // Esperar por la finalización de ambos hijos
+    waitpid(pid1, &status1, 0);
+    waitpid(pid2, &status2, 0);
+
+    printf("Proceso padre esperó a los dos hijos\n");
+
+    return 0;
+}
